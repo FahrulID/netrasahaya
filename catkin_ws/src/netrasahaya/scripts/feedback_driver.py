1680a5d81e9da364bf11836dd80acbe9fa9131ee
FahrulID <fahrulofficial@gmail.com>
2025-01-23 02:17:04 +0700

    fix: feedback driver

--- catkin_ws/src/netrasahaya/scripts/feedback_driver.py
+++ catkin_ws/src/netrasahaya/scripts/feedback_driver.py
@@ -1,6 +1,5 @@
 import rospy
 import serial as ser
-import message_filters
 from std_msgs.msg import Int32
 from typing import List
 
@@ -27,38 +26,7 @@ MOTOR_KANAN = 2
 MOTOR_DEPAN_KIRI = 0
 MOTOR_DEPAN_KANAN = 3
 
-# def callback(kiri: Int32, kanan: Int32, depan_kiri: Int32, depan_kanan: Int32):
-#     global motor_speeds, motor_states
-
-#     if kiri.data == 0:
-#         motor_states[MOTOR_KIRI] = 0
-#     elif kiri.data == 1:
-#         motor_states[MOTOR_KIRI] = 2
-#     elif kiri.data == 2:
-#         motor_states[MOTOR_KIRI] = 1
-
-#     if kanan.data == 0:
-#         motor_states[MOTOR_KANAN] = 0
-#     elif kanan.data == 1:
-#         motor_states[MOTOR_KANAN] = 2
-#     elif kanan.data == 2:
-#         motor_states[MOTOR_KANAN] = 1
-
-#     if depan_kiri.data == 0:
-#         motor_states[MOTOR_DEPAN_KIRI] = 0
-#     elif depan_kiri.data == 1:
-#         motor_states[MOTOR_DEPAN_KIRI] = 2
-#     elif depan_kiri.data == 2:
-#         motor_states[MOTOR_DEPAN_KIRI] = 1
-
-#     if depan_kanan.data == 0:
-#         motor_states[MOTOR_DEPAN_KANAN] = 0
-#     elif depan_kanan.data == 1:
-#         motor_states[MOTOR_DEPAN_KANAN] = 2
-#     elif depan_kanan.data == 2:
-#         motor_states[MOTOR_DEPAN_KANAN] = 1
-
-#     print(f'kiri: {motor_states[MOTOR_KIRI]}, kiri dada: {motor_states[MOTOR_DEPAN_KIRI]}, kanan dada: {motor_states[MOTOR_DEPAN_KANAN]}, kanan: {motor_states[MOTOR_KANAN]}')
+current_mode = "none"
 
 def kiri_cb(msg: Int32):
     global motor_speeds, motor_states
@@ -101,20 +69,12 @@ def depan_kanan_cb(msg: Int32):
         motor_states[MOTOR_DEPAN_KANAN] = 2
 
 if __name__ == '__main__':
-    rospy.init_node('sensing')
-
-    # kiri_sub = message_filters.Subscriber('/kiri', Int32)
-    # kanan_sub = message_filters.Subscriber('/kanan', Int32)
-    # depan_kiri_sub = message_filters.Subscriber('/depan_kiri', Int32)
-    # depan_kanan_sub = message_filters.Subscriber('/depan_kanan', Int32)
+    rospy.init_node('feedback_driver')
 
     kiri_sub = rospy.Subscriber('/kiri', Int32, kiri_cb)
     kanan_sub = rospy.Subscriber('/kanan', Int32, kanan_cb)
     depan_kiri_sub = rospy.Subscriber('/depan_kiri', Int32, depan_kiri_cb)
     depan_kanan_sub = rospy.Subscriber('/depan_kanan', Int32, depan_kanan_cb)
-
-    # ts = message_filters.ApproximateTimeSynchronizer([kiri_sub, kanan_sub, depan_kiri_sub, depan_kanan_sub], 10, 0.1, True)
-    # ts.registerCallback(callback)
         
     s = ser.Serial('/dev/serial/by-id/usb-Arduino__www.arduino.cc__Arduino_14101-if00', 
         baudrate=115200, 
